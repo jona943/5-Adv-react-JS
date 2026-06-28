7905f671b2ba2d3b893825236b8208bdac184ae6
Jonathan Medina <jonathan603015@gmail.com>
2026-06-28 14:04:58 -0700

    Importacion e inicializacion del hook

--- agente-init/src/App.jsx
+++ agente-init/src/App.jsx
@@ -1,9 +1,21 @@
+import { useForm } from "react-hook-form";
 
 function App() {
+  // inicalizacion useForm
+  const { register, handleSubmit, formState: { errors }, } = useForm();
+
+  // Funcion de validacion de envio valido
+  const onSubmit = (data) => {
+    console.log(`Datos enviados: --> ${data}  <-- `);
+    alert(`Datos enviados con exito: --> ${data} <-- `);
+                                      // ${JSON.stringify(data)} --Convercion a string
+  }
+
   return (
     <>
+    {}
     </>
   )
 }
 
-export default App
+export default App;
